355932b88829c731c4e43781c6cf2b3958fdc183
thmastin <thmastin@gmail.com>
2025-04-15 09:51:17 -0400

    Start maze class

---
+++ maze.py
@@ -0,0 +1,19 @@
+class Maze:
+    def __init__(
+        self,
+        x1,
+        y1,
+        num_rows,
+        num_cols,
+        cell_size_x,
+        cell_size_y,
+        win,
+    ):
+        
+        self._x1 = x1
+        self._y1 = y1
+        self._num_rows = num_rows
+        self._num_cols = num_cols
+        self._cell_size_x = cell_size_x
+        self._cell_size_y = cell_size_y
+        self._win = win
